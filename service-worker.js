cdf5ed426ca918d9bb4e75ef98df66a27cb0b886
bhaskar9876 <poolcarromdisc6@gmail.com>
2025-08-23 23:29:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,18 @@
-const CACHE = 'random-dice-v1';
-const ASSETS = ['./','./index.html','./service-worker.js'];
+const CACHE_NAME = "random-dice-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest"
+];
 
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });
